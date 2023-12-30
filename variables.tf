b2d8fe7b434a96ebbdcfa07b0463d10164782a25
Dan Barr <dan@dbarr.com>
2023-12-30 00:58:54 -0500

    Make API Gateway execution logging conditional
    * Requires extra account setup, so defaults to off

--- variables.tf
+++ variables.tf
@@ -8,3 +8,19 @@ variable "log_retention_days" {
   description = "Number of days to retain CloudWatch logs."
   default     = 14
 }
+
+variable "enable_api_gateway_logging" {
+  type        = bool
+  description = "Whether to enable API Gateway logging."
+  default     = false
+}
+
+variable "api_gateway_logging_level" {
+  type        = string
+  description = "Log level for API Gateway execution logging."
+  default     = "ERROR"
+  validation {
+    condition     = contains(["OFF", "ERROR", "INFO"], var.api_gateway_logging_level)
+    error_message = "Invalid logging level specified."
+  }
+}
